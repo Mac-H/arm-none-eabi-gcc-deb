ed70ef64ce7aaee3412061d5d06ef63ca1877ae8
Andrew Leech <andrew.leech@planetinnovation.com.au>
2022-05-29 20:09:13 +1000

    arm-none-eabi-gcc-deb build tagged version.

--- create_deb.sh
+++ create_deb.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # https://askubuntu.com/a/1371525
+# https://developer.arm.com/downloads/-/gnu-rm
 
-VER=15:10.3-2021.10-9
-URL=https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
+VER=${VER:-'10.3-2021.10'}
 
+URL=https://developer.arm.com/-/media/Files/downloads/gnu-rm/${VER}/gcc-arm-none-eabi-${VER}-x86_64-linux.tar.bz2
 
-echo "Creating gcc-arm-none-eabi debian package version $VER"
-echo "Entering temporary directory..."
-cd /tmp
+echo "Creating gcc-arm-none-eabi x86_64 debian package" 
+echo "version: $VER"
 
 echo "Downloading..."
 curl -fSL -A "Mozilla/4.0" -o gcc-arm-none-eabi.tar "$URL"
@@ -28,11 +28,11 @@ echo "Description: Arm Embedded toolchain" >> gcc-arm-none-eabi/DEBIAN/control
 mv gcc-arm-none-eabi-*/* gcc-arm-none-eabi/usr/
 dpkg-deb --build --root-owner-group gcc-arm-none-eabi
 
-echo "Installing..."
-sudo apt install ./gcc-arm-none-eabi.deb -y --allow-downgrades
+# echo "Installing..."
+# sudo apt install ./gcc-arm-none-eabi.deb -y --allow-downgrades
 
-echo "Removing temporary files..."
-rm -r gcc-arm-none-eabi*
+mv "gcc-arm-none-eabi.deb" "gcc-arm-none-eabi-${VER}-x86_64.deb"
+ls -l *.deb
 
 echo "Done."
 
